7a33ace8d103319b537a7cd3e56982caae4aec30
Navneet <navneet.sharma.pro@gmail.com>
2017-10-18 09:20:32 +0530

    life universe
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/

---
+++ lifeUniverse.cpp
@@ -0,0 +1,19 @@
+/**
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    while(a!=42)
+    {
+    cin>>a;
+    if(a!=42)
+    cout<<a<<endl;
+    else continue;
+    }
+    return 0;
+}
+//Silly question got me 3 tries, Fuck !
